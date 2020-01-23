8b7bd2665629b7dded90de5b8c63bfb14abb0527
jevsoest <jeroen.van.soest@vub.be>
2020-01-23 16:17:11 +0100

    first

---
+++ precache-manifest.1050a8e826f8c337589b370550205db0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e33f4efc9664bd426b441ab0e091de3",
+    "url": "/observatories/climate-change-opinion-observatory/index.html"
+  },
+  {
+    "revision": "4cf79e0588d9ab85dd54",
+    "url": "/observatories/climate-change-opinion-observatory/static/css/2.3c2e396e.chunk.css"
+  },
+  {
+    "revision": "4cf79e0588d9ab85dd54",
+    "url": "/observatories/climate-change-opinion-observatory/static/js/2.2d8267b2.chunk.js"
+  },
+  {
+    "revision": "3f09863550eb3e1c5039b86f443859e0",
+    "url": "/observatories/climate-change-opinion-observatory/static/js/2.2d8267b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93db0c1fe31c8911550e",
+    "url": "/observatories/climate-change-opinion-observatory/static/js/main.ea7fda5d.chunk.js"
+  },
+  {
+    "revision": "4dd6d709e594fc8b1518",
+    "url": "/observatories/climate-change-opinion-observatory/static/js/runtime-main.4901d052.js"
+  },
+  {
+    "revision": "a5e39838cd4712bc91e08d628e1ad9fe",
+    "url": "/observatories/climate-change-opinion-observatory/static/media/logo.a5e39838.svg"
+  },
+  {
+    "revision": "4a6f6104bd4b550a2ef6f6905540a043",
+    "url": "/observatories/climate-change-opinion-observatory/static/media/logosmall.4a6f6104.svg"
+  }
+]);
