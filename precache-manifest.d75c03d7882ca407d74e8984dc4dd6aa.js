0326eed4431e485015c0baf9a9fe8dac3910bf3e
jevsoest <jeroen.van.soest@vub.be>
2020-01-24 11:06:03 +0100

    test

---
+++ precache-manifest.d75c03d7882ca407d74e8984dc4dd6aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4578a794b167f9e0b8c0da7b061c3f30",
+    "url": "/observatory-react/index.html"
+  },
+  {
+    "revision": "f34c1057404c9e80a013",
+    "url": "/observatory-react/static/css/2.3c2e396e.chunk.css"
+  },
+  {
+    "revision": "f34c1057404c9e80a013",
+    "url": "/observatory-react/static/js/2.e18b32f3.chunk.js"
+  },
+  {
+    "revision": "3f09863550eb3e1c5039b86f443859e0",
+    "url": "/observatory-react/static/js/2.e18b32f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28ab571821e1781cdf06",
+    "url": "/observatory-react/static/js/main.ad2f2188.chunk.js"
+  },
+  {
+    "revision": "e639b1465f3854dd2c82",
+    "url": "/observatory-react/static/js/runtime-main.dc989a59.js"
+  },
+  {
+    "revision": "a5e39838cd4712bc91e08d628e1ad9fe",
+    "url": "/observatory-react/static/media/logo.a5e39838.svg"
+  },
+  {
+    "revision": "4a6f6104bd4b550a2ef6f6905540a043",
+    "url": "/observatory-react/static/media/logosmall.4a6f6104.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/observatories/climate-change-opinion-observatory/precache-manifest.fcdd5771b2da29e6dae2530b0b1d4d66.js"
+  "/observatory-react/precache-manifest.d75c03d7882ca407d74e8984dc4dd6aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/observatories/climate-change-opinion-observatory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/observatory-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
