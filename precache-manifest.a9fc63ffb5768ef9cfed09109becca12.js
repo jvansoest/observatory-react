6b5f071de5ef424a25ac1ca1642391e518f5ca1c
jevsoest <jeroen.van.soest@vub.be>
2020-04-27 11:43:35 +0200

    website update

---
+++ precache-manifest.a9fc63ffb5768ef9cfed09109becca12.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c2a84bac59ab4f39375ddd3341e231",
+    "url": "/observatory-react/index.html"
+  },
+  {
+    "revision": "0310eda8f2480af47f78",
+    "url": "/observatory-react/static/css/2.3c2e396e.chunk.css"
+  },
+  {
+    "revision": "0310eda8f2480af47f78",
+    "url": "/observatory-react/static/js/2.faaa1925.chunk.js"
+  },
+  {
+    "revision": "495a35d703112f8b9c300d387ab1f548",
+    "url": "/observatory-react/static/js/2.faaa1925.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec24a1e524a9582202e3",
+    "url": "/observatory-react/static/js/main.3346715b.chunk.js"
+  },
+  {
+    "revision": "e639b1465f3854dd2c82",
+    "url": "/observatory-react/static/js/runtime-main.dc989a59.js"
+  },
+  {
+    "revision": "7afe96620158c19059aacee3dc385556",
+    "url": "/observatory-react/static/media/addDataset.7afe9662.png"
+  },
+  {
+    "revision": "7d9c073e4c815962796cfcfbc3c37d7c",
+    "url": "/observatory-react/static/media/causalFrames.7d9c073e.gif"
+  },
+  {
+    "revision": "f8ca984d4dc04e6e717a93a2d91c908b",
+    "url": "/observatory-react/static/media/causalMap.f8ca984d.gif"
+  },
+  {
+    "revision": "fb2d5456b29b5ceeceb25a481322f29a",
+    "url": "/observatory-react/static/media/frameDistribution.fb2d5456.gif"
+  },
+  {
+    "revision": "a5e39838cd4712bc91e08d628e1ad9fe",
+    "url": "/observatory-react/static/media/logo.a5e39838.svg"
+  },
+  {
+    "revision": "4a6f6104bd4b550a2ef6f6905540a043",
+    "url": "/observatory-react/static/media/logosmall.4a6f6104.svg"
+  },
+  {
+    "revision": "4701f06fc18c9a7d56cb28ae3a4f12e1",
+    "url": "/observatory-react/static/media/namedEntities.4701f06f.gif"
+  },
+  {
+    "revision": "0c4b2b748343c514efdddff6f8788f95",
+    "url": "/observatory-react/static/media/plotCounts.0c4b2b74.gif"
+  },
+  {
+    "revision": "1f2ff1e9756c1eb42610a593fdcf3988",
+    "url": "/observatory-react/static/media/screenshotObservatory.1f2ff1e9.png"
+  },
+  {
+    "revision": "2929a9950fb4936727c97a742daaa7b2",
+    "url": "/observatory-react/static/media/statementDistribution.2929a995.gif"
+  },
+  {
+    "revision": "22ae812b1eef044cd8ca6e6c74203d21",
+    "url": "/observatory-react/static/media/statementGraph.22ae812b.gif"
+  },
+  {
+    "revision": "18dd8ebfc3a1fb8cae1761d803a0936a",
+    "url": "/observatory-react/static/media/termFreq.18dd8ebf.gif"
+  },
+  {
+    "revision": "a083eb2296b880c5af3f74b04561890b",
+    "url": "/observatory-react/static/media/wordTree.a083eb22.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/observatories/climate-change-opinion-observatory/precache-manifest.a8d096a86045206a616dc6d0850e083c.js"
+  "/observatory-react/precache-manifest.a9fc63ffb5768ef9cfed09109becca12.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/observatories/climate-change-opinion-observatory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/observatory-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
